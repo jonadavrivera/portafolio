a9c9b762e947bc3a791e2f22b7fe940a6a91d3d6
jonadavrivera <jonadav.rivera@gmail.com>
2025-11-24 20:48:13 -0600

    inicio del proyecto

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="flex flex-col justify-center h-screen px-4">
+            <h1>404 - Página no encontrada</h1>
+        </div>
+    )
+}
